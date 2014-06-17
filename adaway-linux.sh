64f508a908e26f5070eda862012606127f4db3d9
sedrubal <sederic.enders@gmail.com>
2014-06-17 02:12:57 +0200

    initial commit

---
+++ adaway-linux.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# CC BY-SA 4.0
+# https://github.com/sedrubal/adaway-linux
+
+#settings
+tmpdir="/tmp/adaway-linux/"
+hostsorig="$HOME""/.hosts.original"
+#
+
+#show help
+if [ "$1" == "-h" ] || [ "$1" == "--help" ] ; then
+
+  echo "Welcome to adaway-linux, a small script to add domains hosting ads to the hosts file to block them."
+  echo ""
+  echo "[!] please run ./install.sh before using this! It will backup your original hosts-file"
+  echo ""
+  echo "Usage:"
+  echo "You have only to run this script to add the ad-domains to your hosts file or to update them."
+  echo "Parameters:"
+  echo "    -h    --help      show help"
+  echo "    -s    --simulate  simulate, but don't replace hosts-file"
+  echo ""
+  exit 0
+
+fi
+
+#check root
+if [ "$(id -u)" != "0" ] && [ "$1" != "-s" ] && [ "$1" != "--simulate" ] ; then
+echo "This script must be run as root" 1>&2
+exit 1
+fi
+
+#preparing temporary directory
+if [ -d "$tmpdir" ]; then
+  echo "[i] deleting directory $tmpdir"
+  rm -r "$tmpdir"
+fi
+
+echo "[i] creating temporary directory $tmpdir"
+mkdir "$tmpdir"
+touch "$tmpdir"hosts
+
+#fists lines of hosts-file
+echo "[i] add original hosts file from $hostsorig"
+echo "#[!] This file will be updated by the ad-block-script called adaway-linux." >> "$tmpdir"hosts
+echo -e "#[!] If you want to edit the hosts-file, please edit the original file in $hostsorig." >> "$tmpdir"hosts
+echo -e "#[!] Changes will be added to the top of this file.\n\n" >> "$tmpdir"hosts
+cat "$hostsorig" >> "$tmpdir"hosts
+
+#add domains from hosts-server listet in hostssources.lst
+i=0
+while read src; do
+  if [[ $src != "#*" ]] ; then
+    wget -O "$tmpdir$i" $src
+    echo -e "\n---------------------------------\n" >> "$tmpdir"hosts 
+    cat "$tmpdir$((i++))" >> "$tmpdir"hosts
+  else
+    echo "[i] skipping $src"
+  fi
+done <hostssources.lst
+
+#replacing hosts-file
+if [ "$1" != "-s" ] && [ "$1" != "--simulate" ] ; then
+
+  echo "[i] removing old hosts file"
+  rm /etc/hosts
+
+  echo "[i] moving new hosts file to /etc/hosts"
+  mv "$tmpdir"hosts /etc/hosts
+
+  echo "[i] deleting directory $tmpdir"
+  rm -r "$tmpdir"
+
+else
+  echo "[i] skipping replacing the hosts-file. You can see the hosts file there: $tmpdir"hosts
+fi
+
+echo "[i] finished"
+exit 0
---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#settings
+hostsorig="$HOME/.hosts.original"
+srclst="hostssources.lst"
+#
+
+#uninstall
+if [ "$1" == "-u" ] || [ "$1" == "--uninstall" ] ; then
+  echo "[?] Do you really want to uninstall adaway-linux and restore the host-file? [yes]" 
+  read REPLY
+  if [ "$REPLY" == "yes" ] ; then
+    echo "[i] restoring /etc/hosts"
+    sudo mv $hostsorig /etc/hosts
+    echo "[!] if you added a cronjob, pleas remove it yourself!."
+  fi
+
+  echo "[i] finished"
+  exit 0
+fi
+#
+
+
+echo "Welcome to the install-script for adaway-linux."
+
+#backup hosts-file
+echo "[i] First I will backup the original hosts-file to $hostsorig."
+echo "[!] Please run this only ONCE! Abboard, if you modified /etc/hosts by adaway-linux.sh [Enter]"
+read
+sudo cp /etc/hosts ~/.hosts.original
+echo "[!] Please check, if this worked!"
+echo "[!] If this worked, please type yes. Remember that thise file is hidden!"
+read REPLY
+if [ "$REPLY" != "yes" ] ; then
+  echo "[!] Please backup it manually and save it as $hostsorig."
+  exit 1
+fi
+
+#create default hostsources.lst
+echo "[i] Now I will create the $srclst file. There you can add urls, where I can fetch  parts of the hostfile."
+touch "$srclst"
+echo "http://adaway.org/hosts.txt" >> "$srclst"
+echo "http://hosts-file.net/ad_servers.asp" >> "$srclst"
+echo "http://winhelp2002.mvps.org/hosts.txt" >> "$srclst"
+
+#add cronjob
+echo "[?] If you want, I create a cronjob ever 5 days. [y]"
+read REPLY
+if [ "$REPLY" == "y" ] ; then
+  echo "[i] Ok, I'll write in root's cron tab."
+  line="1 12 */5 * * ""$PWD""adaway-linux.sh"
+  (sudo crontab -u root -l; echo "$line" ) | sudo crontab -u root -
+fi
+
+echo "[i] finished. For uninstall, please run ./install.sh -u"
+exit 1
